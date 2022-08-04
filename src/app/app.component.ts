2ca2af7f09a475c9d2330212c2015ce4bc7b6dfc
elkhwatry <mahmoudelkhwatry20699@gmail.com>
2022-08-04 06:34:44 -0700

    new task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'salem-project';
+  facilities =  [
+    {
+        "facilityId": 1,
+        "facilityCode": "test",
+        "facilityEnglishName": "test",
+        "facilityArabicName": "test",
+        "facilityType": 1,
+        "facilityNetwork": 1,
+        "activeFacility": 1
+    },
+    {
+        "facilityId": 2,
+        "facilityCode": "5",
+        "facilityEnglishName": "api",
+        "facilityArabicName": "api",
+        "facilityType": 5,
+        "facilityNetwork": 5,
+        "activeFacility": 0
+    },
+    {
+        "facilityId": 3,
+        "facilityCode": "5",
+        "facilityEnglishName": "api",
+        "facilityArabicName": "api",
+        "facilityType": 5,
+        "facilityNetwork": 5,
+        "activeFacility": 0
+    },
+    {
+        "facilityId": 4,
+        "facilityCode": "5",
+        "facilityEnglishName": "api",
+        "facilityArabicName": "api",
+        "facilityType": 5,
+        "facilityNetwork": 5,
+        "activeFacility": 0
+    },
+    {
+        "facilityId": 5,
+        "facilityCode": "5",
+        "facilityEnglishName": "api",
+        "facilityArabicName": "api",
+        "facilityType": 5,
+        "facilityNetwork": 5,
+        "activeFacility": 0
+    },
+    {
+        "facilityId": 6,
+        "facilityCode": "5",
+        "facilityEnglishName": "api",
+        "facilityArabicName": "api",
+        "facilityType": 5,
+        "facilityNetwork": 5,
+        "activeFacility": 0
+    },
+    {
+        "facilityId": 7,
+        "facilityCode": "5",
+        "facilityEnglishName": "api",
+        "facilityArabicName": "api",
+        "facilityType": 5,
+        "facilityNetwork": 5,
+        "activeFacility": 0
+    }
+]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
